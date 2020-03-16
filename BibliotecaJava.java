3ee722d826c0f7eeb1ffb886edb2b817d990a900
Raul Rodriguez <61161518+ruliz07@users.noreply.github.com>
2020-03-16 16:57:40 -0600

    Main Principal con Formulario
    Main Principal con Interfaz de Formulario uso de librerías y clases principales

---
+++ BibliotecaJava.java
@@ -0,0 +1,13 @@
+
+package bibliotecajava;
+
+import java.awt.Color;
+import javax.swing.JOptionPane;
+
+public class BibliotecaJava {
+    public static void main(String[] args) {      
+        System.out.println("Prueba del proyecto final de Angel Wha");
+        MenuPrincipal v1 = new MenuPrincipal();
+        //v1.setBackground(new Color(255,0,255)); //para cambiar color
+    }
+}
